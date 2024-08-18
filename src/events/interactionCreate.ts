6f184c67c91e6b201198b2f3485d312f651ceedf
Sravdar <Sravdar@users.noreply.github.com>
2024-08-18 15:43:44 +0300

    Fixed duplicated console logs for interactions

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -4,6 +4,15 @@ import { DiscordClient } from '../classes/discordClient';
 async function execute(interaction: BaseInteraction) {
 	const client = interaction.client as DiscordClient;
 
+	let interactionChannelName = 'Unknown Channel';
+	if (interaction.channel?.type == ChannelType.DM) {
+		interactionChannelName = 'DM Channel';
+	} else if (interaction.channel) {
+		interactionChannelName = interaction.channel.name;
+	} else {
+		interactionChannelName = 'Unknown Channel';
+	}
+
 	// Handling button commands, no idea how this was working -Sravdar
 	// Should be only working with buttons in interaction reply of slashcommand
 	if (interaction.isButton() && interaction.message.interaction) {
@@ -13,17 +22,18 @@ async function execute(interaction: BaseInteraction) {
 			return;
 		}
 		try {
+			console.log(`${interaction.user.tag} in #${interactionChannelName} triggered an button "${interaction.message.interaction.commandName}" interaction`);
 			await command.buttonExecute(interaction);
-			console.log(`${interaction.user.tag} triggered an select button interaction.`);
 		} catch (error) {
-			console.error(`Error executing ${interaction.message.interaction.commandName}`);
+			console.error(`Error executing ${interaction.message.interaction.commandName} requested by ${interaction.user.tag}`);
 			console.error(error);
 		}
 	}
 
 	// handling select menu commands
+	// TODO: Make this modular just like its done in button commands
 	if (interaction.isStringSelectMenu()) {
-		console.log(`${interaction.user.tag} triggered an select menu interaction.`);
+		console.log(`${interaction.user.tag} in #${interactionChannelName} triggered an select menu interaction`);
 		if (interaction.customId === 'selectSong') {
 			const searchCommand = client.commands.get('ara');
 			try {
@@ -34,36 +44,19 @@ async function execute(interaction: BaseInteraction) {
 			}
 		}
 	}
-	// return if it isn't a normal command.
-	if (!interaction.isCommand()) return;
-	// down below is normal command options
-	let interactionChannelName = 'Unknown Channel';
-	if (interaction.channel?.type == ChannelType.DM) {
-		interactionChannelName = `${interaction.channel.recipient?.username} DM Channel`;
-	} else if (interaction.channel) {
-		interactionChannelName = interaction.channel.name;
-	} else {
-		interactionChannelName = 'Unknown Channel';
-	}
 
-
-	if (interaction.isUserContextMenuCommand()) {
-		console.log(`${interaction.user.tag} in #${interactionChannelName} triggered an user context menu interaction.`);
-	} else if (interaction.inGuild()) {
-		console.log(`${interaction.user.tag} in #${interactionChannelName} triggered an interaction.`);
-	} else {
-		console.log(`${interaction.user.tag} in direct message channel triggered an interaction.`);
-	}
-	// Retrieving command from the commands Collection by using its key that is its name.
-	const command = client.commands.get(interaction.commandName);
-	if (!command) return;
-
-	try {
-		console.log(`${interaction.user.tag} triggered an a normal command interaction.`);
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'Bir hata oluştu!', ephemeral: true });
+	// Handle normal slash commands
+	if (interaction.isCommand()) {
+		const command = client.commands.get(interaction.commandName);
+		if (command) {
+			try {
+				console.log(`${interaction.user.tag} in #${interactionChannelName} triggered an a slash command "${interaction.commandName}" interaction.`);
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				await interaction.reply({ content: 'Bir hata oluştu!', ephemeral: true });
+			}
+		}
 	}
 }
 
